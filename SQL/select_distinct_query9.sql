bfbe00e5eb8cb021196c8fe795789247b84ac1ea
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-05-09 22:59:59 +0530

    Add files via upload

---
+++ SQL/select_distinct_query9.sql
@@ -0,0 +1,3 @@
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT city FROM station WHERE NOT (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' OR city LIKE 'U%');
