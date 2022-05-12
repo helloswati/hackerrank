b6a20584edf96229b00fc1ba78a76bf9aada78fd
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-05-12 23:12:55 +0530

    Update text_wrap.py

--- python/text_wrap.py
+++ python/text_wrap.py
@@ -1,3 +1,17 @@
+You are given a string S  and width w.
+Your task is to wrap the string into a paragraph of width W.
+Function Description
+Complete the wrap function in the editor below.
+wrap has the following parameters:
+String string: a long string
+int max_width: the width to wrap to
+Returns
+string: a single string with newline characters ('\n') where the breaks should be
+
+
+
+
+
 import textwrap
 
 def wrap(string, max_width):
