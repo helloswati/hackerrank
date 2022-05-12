3646b4a5880a4405c2a2680278e736a71b5e397d
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-05-12 23:10:48 +0530

    Update Designer_Door_Mat.py

--- python/Designer_Door_Mat.py
+++ python/Designer_Door_Mat.py
@@ -1,3 +1,12 @@
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+Mat size must be N X M.(N is an odd natural number, and M is 3 times N.)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+
+    
+
+
+
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 N, M = map(int, input().split())
 for i in range(1, N, 2):
