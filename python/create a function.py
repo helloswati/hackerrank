67353133bc5f4cd11fb484d705477f69b62dcc31
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-05-27 22:31:20 +0530

    Update create a function.py

--- python/create a function.py
+++ python/create a function.py
@@ -7,3 +7,10 @@ Answers : 2. y = 0
              while x > 10 :
              y = y * 10 + x % 10
              x = x // 10
+
+Answer : 3. def reverse (x):
+            y = 0
+            while x % 10 > 0:
+            x < x // 10
+            return y 
+          
