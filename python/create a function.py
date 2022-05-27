31d8de6cba81387f976c0693cbb427cc8e114a1b
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-05-27 22:08:03 +0530

    Add files via upload

--- python/create a function.py
+++ python/create a function.py
@@ -3,16 +3,7 @@ python program to create a function :
 2. is.pallindrome (x) returns if x is a pallindrome or not 
 3. reverse (x) returns the reverse of the numbers (x)
 
-Answers : 3. y = 0
+Answers : 2. y = 0
              while x > 10 :
              y = y * 10 + x % 10
              x = x // 10
-
-Answer : 2. def reverse (x):
-            y = 0
-            while x % 10 + x % 10
-            x < x // 10
-            return y
-
-Answer : 1. sum_result = d.sum(2, 3)
-            print(sum_result)
