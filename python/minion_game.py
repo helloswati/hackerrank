3823c017b508ea373aedc81e047f9d7bb9fcd4a9
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-05-28 22:25:07 +0530

    Add files via upload

--- python/minion_game.py
+++ python/minion_game.py
@@ -1,13 +1,5 @@
-Kevin and Stuart want to play the 'The Minion Game'.
-Game Rules
-Both players are given the same string,S.
-Both players have to make substrings using the letters of the string S  .
-Stuart has to make words starting with consonants.
-Kevin has to make words starting with vowels.
-The game ends when both players have made all possible substrings. 
 
 
-Solution :
 def minion_game(string):
     # your code goes here
     vowel = 'aeiou'.upper()
