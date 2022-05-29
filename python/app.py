0cc1d3b8f4ab5f29c2ca7074cc0406f96132de61
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-05-29 23:33:24 +0530

    Add files via upload

---
+++ python/app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/menu")
+def hello_world():
+    '''
+    Method
+    '''
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/swati')
+def swati():
+    '''
+    Returns Swati
+    '''
+    return ("Swati")
+
+
+@app.route('/index')
+def index():
+    '''
+    Index
+    '''
+    return render_template('index.html')
+
+
+@app.route('/sum')
+def sum():
+    '''
+    Returns the sum
+    '''
+    args = request.args
+    # print (args)
+    a = int(args.get('a'))
+    b = int(args.get('b'))
+    sum = (a + b)
+    return (str(sum))
+
+
+@app.route('/subtract')
+def subtract():
+    '''
+    Returns the subtract
+    '''
+    args = request.args
+    # print (args)
+    a = int(args.get('a'))
+    b = int(args.get('b'))
+    result = (a - b)
+    return (str(result))
+
+
+@app.route('/product')
+def product():
+    '''
+    Returns the product
+    '''
+    args = request.args
+    # print (args)
+    a = int(args.get('a'))
+    b = int(args.get('b'))
+    result = (a * b)
+    return (str(result))
+
+
+@app.route('/divide', methods=['GET'])
+def divide():
+    '''
+    Returns the divide
+    '''
+    args = request.args
+    # print (args)
+    a = int(args.get('a'))
+    b = int(args.get('b'))
+    result = (a / b)
+    return (str(result))
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=8087, debug=True)
