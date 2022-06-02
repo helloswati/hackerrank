c60663eee8bb708dacbfe5edea0a4fd91f6de504
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-02 23:08:43 +0530

    Add files via upload

--- python/itertools.product().py
+++ python/itertools.product().py
@@ -1,13 +1,4 @@
-itertools.product()
-
-This tool computes the cartesian product of input iterables.
-It is equivalent to nested for-loops.
-For example, product(A, B) returns the same as ((x,y) for x in A for y in B). 
-
-
-
 from itertools import product 
 a = map(int, input().split())
 b = map(int, input().split())
 print(*product(a, b))
-
