a20c386aa21e5d615bb6ff9b9730a6229eabb404
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-03 20:13:04 +0530

    Update itertools _import_ permutations.py

--- python/itertools _import_ permutations.py
+++ python/itertools _import_ permutations.py
@@ -1,6 +1,6 @@
 itertools.permutations(iterable[, r])
 This tool returns successive r length permutations of elements in an iterable.
-If is not specified or is None, then defaults to the length of the iterable, and all possible full length permutations are generated.
+If r is not specified or is None, then r defaults to the length of the iterable, and all possible full length permutations are generated.
 Permutations are printed in a lexicographic sorted order. So, if the input iterable is sorted, the permutation tuples will be produced in a sorted order.
 
 
