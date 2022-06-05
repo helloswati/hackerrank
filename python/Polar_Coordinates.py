81c1543a6f7f5d46e1d3e567d95643cd7e6d8fce
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-05 22:35:06 +0530

    Update Polar_Coordinates.py

--- python/Polar_Coordinates.py
+++ python/Polar_Coordinates.py
@@ -1,5 +1,6 @@
 Polar coordinates are an alternative way of representing Cartesian coordinates or Complex Numbers.
 A complex number z [Capture.PNG] 
+    z = x + yj
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import cmath
