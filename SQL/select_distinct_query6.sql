894391534a36ee2f2997fb7cb7bf53c3379eac78
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-07 16:36:24 +0530

    Update select_distinct_query6.sql

--- SQL/select_distinct_query6.sql
+++ SQL/select_distinct_query6.sql
@@ -1,3 +1,5 @@
 Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
 
+
+Solution:
 SELECT DISTINCT city FROM station WHERE city LIKE ‘A%’ OR city LIKE ‘E%’ OR city LIKE ‘I%’ OR city LIKE ‘O%’ OR city LIKE ‘U%’;
