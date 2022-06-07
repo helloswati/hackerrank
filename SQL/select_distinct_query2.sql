af7b1edda73acbfb3c9f47fd0387ca4920478a63
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-07 16:34:28 +0530

    Update select_distinct_query2.sql

--- SQL/select_distinct_query2.sql
+++ SQL/select_distinct_query2.sql
@@ -1,8 +1,9 @@
-Let N be the number of CITY entries in STATION, and let N’ be the number of distinct CITY names in STATION; query the value of N-N’ from STATION. In other words, find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+Let N be the number of CITY entries in STATION, and let N’ be the number of distinct CITY names in STATION; query the value of N-N’ from STATION. 
+In other words, find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 
 where LAT_N is the northern latitude and LONG_W is the western longitude.
 
 
 
-
+Solution:
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION ;
