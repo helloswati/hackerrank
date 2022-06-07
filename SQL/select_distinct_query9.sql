9d1fa321fc84072e0924a33aef7432af0b727ef0
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-07 16:34:54 +0530

    Update select_distinct_query9.sql

--- SQL/select_distinct_query9.sql
+++ SQL/select_distinct_query9.sql
@@ -1,3 +1,4 @@
 Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 
+Solution:
 SELECT DISTINCT city FROM station WHERE NOT (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' OR city LIKE 'U%');
