3de96d760d5b8cac0aac86816cb3831cd03e96c5
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-08 19:17:27 +0530

    Update select_distinct_query3.sql

--- SQL/select_distinct_query3.sql
+++ SQL/select_distinct_query3.sql
@@ -1,4 +1,6 @@
 Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
-The STATION table is described as follows
+The STATION table is described as follows.
 
+
+Solution:
 SELECT COUNT(CITY) — COUNT(DISTINCT CITY) FROM STATION ;
