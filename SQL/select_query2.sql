f90331aca7b26f19fdb5d623fb3f69202f6a47bf
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-09 14:36:10 +0530

    Update select_query2.sql

--- SQL/select_query2.sql
+++ SQL/select_query2.sql
@@ -1,4 +1,5 @@
 Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
 The CITY table is described as follows: 
 
+Solution:
 select name from City where Countrycode = "USA" and Population >120000
