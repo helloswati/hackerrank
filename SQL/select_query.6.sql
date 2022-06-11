cf4619443ffbc5671b4b4c9757475219387245ec
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-11 22:27:51 +0530

    Update select_query.6.sql

--- SQL/select_query.6.sql
+++ SQL/select_query.6.sql
@@ -1,4 +1,5 @@
 Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
 The CITY table is described as follows: 
 
+Solution:
 select name from City where COUNTRYCODE = 'JPN'
