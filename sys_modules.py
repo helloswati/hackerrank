18a109ab62b9dcee0b77ce43b17acfe72faaa978
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-14 12:41:59 +0530

    Add files via upload

---
+++ sys_modules.py
@@ -0,0 +1,123 @@
+# The sys module in Python provides various functions and variables that are used to 
+# to manipulate different parts of the Python runtime environment
+
+import sys
+  
+print(sys.version)
+
+# stdin
+
+import sys
+for line in sys.stdin:
+	if 'q' == line.rstrip():
+		break
+	print(f'Input : {line}')
+
+print("Exit")
+
+
+# stdout
+
+import sys
+sys.stdout.write('Geeks')
+
+
+# stderr
+
+import sys
+
+def print_to_stderr(*a):
+
+	# Here a is the array holding the objects
+	# passed as the argument of the function
+	print(*a, file = sys.stderr)
+
+print_to_stderr("Hello World")
+
+## Command Line Arguments
+# It is a list of command-line arguments.
+# len(sys.argv) provides the number of command-line arguments.
+# sys.argv[0] is the name of the current Python script.
+
+# sys.argv
+# Python program to demonstrate
+# command line arguments
+
+import sys
+
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+	print(sys.argv[i], end = " ")
+	
+# Addition of numbers
+Sum = 0
+
+for i in range(1, n):
+	Sum += int(sys.argv[i])
+	
+print("\n\nResult:", Sum)
+
+
+# Exiting the Program
+## sys.exit([arg])
+
+# Python program to demonstrate
+# sys.exit()
+
+import sys
+
+age = 17
+
+if age < 18:
+	
+	# exits the program
+	sys.exit("Age less than 18")	
+else:
+	print("Age is not less than 18")
+
+
+
+# Working with Modules
+## sys.path is an ordinary list and can be manipulated.
+
+import sys
+print(sys.path)
+
+# Truncating the value of sys.path
+
+import sys
+
+# Removing the values
+sys.path = []
+
+# importing pandas after removing
+# values
+import pandas
+
+
+# sys.modules return the name of the Python modules that the current shell has imported.
+
+import sys
+
+print(sys.modules)
+
+# Reference Count
+## sys.getrefcount() 
+
+import sys
+
+a = 'Geeks'
+
+print(sys.getrefcount(a))
+
+
+
+
+
