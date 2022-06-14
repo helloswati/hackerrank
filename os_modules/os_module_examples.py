46276763dacfa6b88a4f58933db73303017acad1
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-06-14 10:33:50 +0530

    Add files via upload

---
+++ os_modules/os_module_examples.py
@@ -0,0 +1,22 @@
+import os
+
+# Function to Get the current 
+# working directory
+def current_path():
+    print(os.getcwd())
+
+
+# Print current working directory
+print ("Current Working Directory is ", os.getcwd())
+
+
+
+# Changing the CWD
+os.chdir('../')
+   
+# Printing CWD after
+print ("Current Working Directory is ", os.getcwd())
+
+directory = "GeeksforGeeks"
+
+
