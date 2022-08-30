4eb0bb9f7d4a4ca2a32a55e37f64511b3a09599c
Swati Gupta <97786716+helloswati@users.noreply.github.com>
2022-08-30 20:23:20 +0530

    Add files via upload

---
+++ python/Python_For_Loops.py
@@ -0,0 +1,22 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+# The break Statement
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+
+# The continue Statement
+
+ fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+  
